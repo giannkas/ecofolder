e9f1c8f81376bd8c875ae11ef240f9ab6c206fba
Giann Karlo <giannkas1@gmail.com>
2023-05-30 19:32:03 +0200

    playing with bad states on the Gandalf paper examples, next step is to integrate a SAT solver to reach bad states from a well-known bad one. No segmentation fault found!

--- shownet.sh
+++ shownet.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-echo "Unfolding..."
-#./ecofolder $1.ll_net
-./ecofolder -c -mcmillan $1.ll
-
 echo "Converting to dot..."
-./mci2dot_cpr $1_unf.mci > $1_unf.dot
+#./llnet2dot $1.ll_net
+./llnet2dot $1.ll_net
 
 echo "Producing the pdf..."
-dot -T pdf $1_unf.dot > $1_unf.pdf
+dot -T pdf $1_net.dot > $1_net.pdf
 
 echo "Displaying the pdf..."
-evince $1_unf.pdf
+evince $1_net.pdf
 
 #rm -f $1.ll_net $1.dot $1.pdf $1.mci
 #rm -f $1.ll_net $1.dot $1.mci
