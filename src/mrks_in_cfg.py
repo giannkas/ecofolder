af0ab67b055deef326a5c7b79a9580ed57fb7d8e
Giann Karlo <giannkas1@gmail.com>
2024-09-06 13:56:10 +0200

    mrks_in_cfg module can now update events in evev file that produce already seen markings

--- src/mrks_in_cfg.py
+++ src/mrks_in_cfg.py
@@ -39,26 +39,29 @@ def repeated_markings_in_conf():
       evev_path = sys.argv[i]
 
   # Read 'evev' file
-  with open(evev_path, "r") as file:
+  with open(evev_path, "r+") as file:
     evev_lines = file.readlines()
 
-  newevev = [[] for conf in range(len(evev_lines))]
+    newevev = [[] for _ in range(len(evev_lines))]
 
-  for conf_number, conf in enumerate(evev_lines):
-    known = []
-    conf = conf.split(' ')[:-1]
-    for ev_number, ev in enumerate(conf,start=1):
-      args_mrk = [script_path("mci2dot"), "-cf", ",".join(e for e in conf[:ev_number]), model_unf]
-      mrk = subprocess.run(args_mrk,capture_output=True,text=True).stdout.strip()
-      ev_pos = ev_number-1
-      set_mrk = {pl for pl in mrk.split(',')}
-      if set_mrk in known:
-        newevev[conf_number].append(conf[ev_pos]+'+')
-      else:
-        newevev[conf_number].append(conf[ev_pos])
-        known.append(set_mrk)
+    for conf_number, conf in enumerate(evev_lines):
+      known = []
+      conf = conf.split(' ')[:-1]
+      for ev_number, _ in enumerate(conf,start=1):
+        args_mrk = [script_path("mci2dot"), "-cf", ",".join(e for e in conf[:ev_number]), model_unf]
+        mrk = subprocess.run(args_mrk,capture_output=True,text=True).stdout.strip()
+        ev_pos = ev_number-1
+        set_mrk = {pl for pl in mrk.split(',')}
+        if set_mrk in known:
+          newevev[conf_number].append(conf[ev_pos]+'+')
+        else:
+          newevev[conf_number].append(conf[ev_pos])
+          known.append(set_mrk)
 
-  print(newevev)  
+    file.seek(0)
+    for conf in newevev:
+      file.write(" ".join(e for e in conf))
+      file.write(" 0\n")
 
 if __name__ == "__main__":
   repeated_markings_in_conf()
